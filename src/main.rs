5f962961e70ae62e393fc4cff699a79cfec8910c
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2021-04-11 20:20:00 +0100

    opcodes and disassembler (14.4)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+use std::convert::TryFrom;
+
+#[derive(IntoPrimitive, TryFromPrimitive)]
+#[repr(u8)]
+enum OpCode {
+    Return,
+}
+
+type Chunk = Vec<u8>;
+
+fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
+    print!("{:04} ", offset);
+    let byte = chunk[offset];
+    match OpCode::try_from(byte) {
+        Ok(instruction) => match instruction {
+            OpCode::Return => simple_instruction("RETURN", offset),
+        },
+        Err(_) => {
+            println!("Unknown opcode {}", byte);
+            offset + 1
+        }
+    }
+}
+
+fn simple_instruction(name: &str, offset: usize) -> usize {
+    println!("{}", name);
+    offset + 1
+}
+
+fn disassemble_chunk(chunk: &Chunk, name: &str) {
+    println!("== {} ==", name);
+    let mut offset = 0;
+    while offset < chunk.len() {
+        offset = disassemble_instruction(&chunk, offset);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut chunk: Chunk = Vec::new();
+    chunk.push(OpCode::Return.into());
+    disassemble_chunk(&chunk, "test chunk");
 }
