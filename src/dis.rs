d6b7254ea0e58f7a72f349f81ff2e14799e8da4d
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2021-04-17 21:26:28 +0100

    moved disassembler into separate file

---
+++ src/dis.rs
@@ -0,0 +1,48 @@
+use crate::{Chunk, LineNo, OpCode};
+use std::convert::TryFrom;
+
+fn disassemble_instruction(chunk: &Chunk, offset: usize, line: Option<LineNo>) -> usize {
+    match line {
+        None => print!("    | {:04} ", offset),
+        Some(l) => print!("{:5} {:04} ", l, offset),
+    }
+    let byte = chunk.code[offset];
+    match OpCode::try_from(byte) {
+        Ok(instruction) => match instruction {
+            OpCode::Constant => constant_instruction("CONSTANT", chunk, offset),
+            OpCode::Return => simple_instruction("RETURN", offset),
+        },
+        Err(_) => {
+            println!("Unknown opcode {}", byte);
+            offset + 1
+        }
+    }
+}
+
+fn simple_instruction(name: &str, offset: usize) -> usize {
+    println!("{}", name);
+    offset + 1
+}
+
+fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
+    let constant_index = chunk.code[offset + 1];
+    print!("{:<16} {:<4} ", name, constant_index);
+    println!("{}", chunk.constants[constant_index as usize]);
+    offset + 2
+}
+
+pub(crate) fn disassemble_chunk(chunk: &Chunk, name: &str) {
+    println!("== {} ==", name);
+    let mut new_lines = chunk.lines.iter().peekable();
+    let mut offset = 0;
+    while offset < chunk.code.len() {
+        let line = match new_lines.peek() {
+            Some(&&(offs, l)) if offs == offset => {
+                new_lines.next();
+                Some(l)
+            }
+            _ => None,
+        };
+        offset = disassemble_instruction(&chunk, offset, line);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use num_enum::{IntoPrimitive, TryFromPrimitive};
-use std::convert::TryFrom;
+
+mod dis;
 
 #[derive(IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
@@ -40,52 +41,6 @@ impl Chunk {
     }
 }
 
-fn disassemble_instruction(chunk: &Chunk, offset: usize, line: Option<LineNo>) -> usize {
-    match line {
-        None => print!("    | {:04} ", offset),
-        Some(l) => print!("{:5} {:04} ", l, offset),
-    }
-    let byte = chunk.code[offset];
-    match OpCode::try_from(byte) {
-        Ok(instruction) => match instruction {
-            OpCode::Constant => constant_instruction("CONSTANT", chunk, offset),
-            OpCode::Return => simple_instruction("RETURN", offset),
-        },
-        Err(_) => {
-            println!("Unknown opcode {}", byte);
-            offset + 1
-        }
-    }
-}
-
-fn simple_instruction(name: &str, offset: usize) -> usize {
-    println!("{}", name);
-    offset + 1
-}
-
-fn constant_instruction(name: &str, chunk: &Chunk, offset: usize) -> usize {
-    let constant_index = chunk.code[offset + 1];
-    print!("{:<16} {:<4} ", name, constant_index);
-    println!("{}", chunk.constants[constant_index as usize]);
-    offset + 2
-}
-
-fn disassemble_chunk(chunk: &Chunk, name: &str) {
-    println!("== {} ==", name);
-    let mut new_lines = chunk.lines.iter().peekable();
-    let mut offset = 0;
-    while offset < chunk.code.len() {
-        let line = match new_lines.peek() {
-            Some(&&(offs, l)) if offs == offset => {
-                new_lines.next();
-                Some(l)
-            }
-            _ => None,
-        };
-        offset = disassemble_instruction(&chunk, offset, line);
-    }
-}
-
 fn main() {
     let mut chunk = Chunk::new();
     let constant_index = chunk.add_constant(1.2);
@@ -93,5 +48,5 @@ fn main() {
     chunk.write(constant_index, 122);
     chunk.write(OpCode::Return.into(), 122);
     chunk.write(OpCode::Return.into(), 123);
-    disassemble_chunk(&chunk, "test chunk");
+    dis::disassemble_chunk(&chunk, "test chunk");
 }
